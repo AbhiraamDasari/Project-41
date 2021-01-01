b13d76a122f7220b061ef44c454bd1c8d342d832
AbhiraamDasari <71956827+AbhiraamDasari@users.noreply.github.com>
2021-01-01 10:34:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+ var raindrops = [];
+ var batman;
+var thunder,thunderimg;
+
+function preload(){
+    thunderimg = loadImage("1.png");
+    thunderimg2 = loadImage("2.png");
+    thunderimg3= loadImage("3.png");
+    thunderimg4 = loadImage("4.png");
+
+}
+
+function setup(){
+    engine = Engine.create();
+    world = engine.world;
+    var canvas = createCanvas(950,500);
+   batman = new Batman(400, 400, 225);
+   thunder = createSprite(600,5,10,40);
+   thunder2 = createSprite(250,2,10,10);
+   thunder3 = createSprite(600,5,10,40);
+   thunder4 = createSprite(600,5,10,40);
+}
+
+function draw(){
+    background("black");
+  batman.display(); 
+
+  if(frameCount % 1 === 0){
+     var raindrop = new RainDrops(random(0, windowWidth), -100,10);
+    raindrops.push(raindrop);
+  }
+  if(frameCount % 10 === 0) {
+    var rand = Math.round(random(1,2));
+      switch(rand) {
+        case 1:thunder.addImage(thunderimg);
+               thunder.scale = 2;
+                break;
+        case 2:thunder.addImage(thunderimg2);
+               thunder.scale = 1.3;
+                break;
+        case 3:thunder.addImage(thunderimg3);
+               thunder.scale = 3;
+                break; 
+        case 4:thunder.addImage(thunderimg4);
+               thunder.scale = 2.4;
+                break;               
+         default: break;
+      }
+    }   
+    if(frameCount % 10 === 0) {
+      var rand = Math.round(random(1,2));
+        switch(rand) {
+          case 1:thunder2.addImage(thunderimg2);
+                 thunder2.scale = 2;
+                  break;
+          case 2:thunder2.addImage(thunderimg);
+                 thunder2.scale = 1.7;
+                  break;
+           default: break;
+        }
+      }   
+
+  for(var i = 0; i < raindrops.length; i++){
+    raindrops[i].fall(40);
+    raindrops[i].velocityY++
+    raindrops[i].display();
+  }
+  drawSprites();
+}   
+
